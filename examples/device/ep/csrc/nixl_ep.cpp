f96ecad6da4cea80f22dbd9e4e6e3947e18afa44
Micha Dery <mdery@nvidia.com>
2025-12-11 14:55:17 -0800

    Mask setting for connected nodes

--- examples/device/ep/csrc/nixl_ep.cpp
+++ examples/device/ep/csrc/nixl_ep.cpp
@@ -353,6 +353,8 @@ void Buffer::connect_ranks(const std::vector<int>& remote_ranks_list, const std:
         if (remote_mds.has_value())
             new_ranks_mds.push_back((*remote_mds)[i]);
 
+        CUDA_CHECK(cudaMemset(mask_buffer_ptr + remote_rank, 0, sizeof(int)));
+
         if (enable_shrink) {
             _nixl_ep_barrier_buffer_clear(remote_rank);
         }
