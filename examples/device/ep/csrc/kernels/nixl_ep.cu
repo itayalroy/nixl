3f194cc834f03e8c1e1001cf97fd21bbc732a2a1
Itay Alroy <ialroy@nvidia.com>
2026-01-05 00:35:07 +0200

    fix another tma store wait
    Signed-off-by: Itay Alroy <ialroy@nvidia.com>

--- examples/device/ep/csrc/kernels/nixl_ep.cu
+++ examples/device/ep/csrc/kernels/nixl_ep.cu
@@ -760,6 +760,7 @@ combine(void* combined_x,
                     }
 
                     // Flush all stores
+                    tma_store_wait<0>();
                     __syncwarp();
                 }
 
