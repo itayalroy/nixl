bdf43621a3fa36f4c7bd0b3f8868f70033c95ecd
Itay Alroy <ialroy@nvidia.com>
2026-01-06 13:56:21 +0200

    fix print
    Signed-off-by: Itay Alroy <ialroy@nvidia.com>

--- examples/device/ep/csrc/kernels/nixl_ep.cu
+++ examples/device/ep/csrc/kernels/nixl_ep.cu
@@ -374,7 +374,7 @@ dispatch(void* packed_recv_x, void* packed_recv_x_scales,
     if (thread_id == 0 && sm_id < num_local_experts) {
         const int total_tokens = packed_recv_count[sm_id];
         if (total_tokens > 1024) {
-            printf("[NIXL_EP-DISPATCH] ERROR: Rank %d, local_expert %d received %d tokens (exceeds 256)\n",
+            printf("[NIXL_EP-DISPATCH] ERROR: Rank %d, local_expert %d received %d tokens (exceeds 1024)\n",
                    rank, sm_id, total_tokens);
         } else if (total_tokens == 1024) {
             printf("[NIXL_EP-DISPATCH] WARNING: Rank %d, local_expert %d received %d tokens (approaching limit)\n",
