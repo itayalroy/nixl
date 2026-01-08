25b34b4503593a21cfe9a2278e434e443dfa0d02
Itay Alroy <ialroy@nvidia.com>
2026-01-08 17:19:15 +0200

    Fix upstream fixes
    Signed-off-by: Itay Alroy <ialroy@nvidia.com>

--- examples/device/ep/csrc/kernels/nixl_ep.cu
+++ examples/device/ep/csrc/kernels/nixl_ep.cu
@@ -767,6 +767,7 @@ combine(void* combined_x,
                     }
 
                     // Flush all stores
+                    tma_store_wait<0>();
                     __syncwarp();
                 }
 
@@ -983,9 +984,6 @@ combine(void* combined_x,
                 __syncwarp();
             }
         }
-
-        // Flush all stores
-        tma_store_wait<0>();
     }
 }
 
