c6bcc506ffe43bc02d1280233ab6f03a8f92a03f
Itay Alroy <ialroy@nvidia.com>
2026-01-16 00:47:37 +0200

    dummy buffers hack
    Signed-off-by: Itay Alroy <ialroy@nvidia.com>

--- examples/device/ep/csrc/nixl_ep.cpp
+++ examples/device/ep/csrc/nixl_ep.cpp
@@ -674,8 +674,9 @@ void Buffer::_nixl_ep_connect_ranks(const std::vector<int>& ranks) {
     std::unordered_set<int> remote_set(remote_ranks.begin(), remote_ranks.end());
     for (int r = 0; r < max_num_ranks; r++) {
         bool valid = remote_set.count(r);
-        remote_descs.addDesc(valid ? nixlRemoteDesc((uintptr_t)nixl_peer_info[r].rdma_buffer_ptr, num_rdma_bytes, nixl_peer_info[r].device_id, nixl_agent_info->remote_agent_names[r]) : nixlRemoteDesc(true));
-        barrier_descs.addDesc(valid ? nixlRemoteDesc((uintptr_t)nixl_peer_info[r].sync_buffer_ptr, max_num_ranks * sizeof(int), nixl_peer_info[r].device_id, nixl_agent_info->remote_agent_names[r]) : nixlRemoteDesc(true));
+        int idx = (r == rank) ? (r + 1) % max_num_ranks : r;
+        remote_descs.addDesc(nixlRemoteDesc((uintptr_t)nixl_peer_info[idx].rdma_buffer_ptr, num_rdma_bytes, nixl_peer_info[idx].device_id, nixl_agent_info->remote_agent_names[idx]));
+        barrier_descs.addDesc(nixlRemoteDesc((uintptr_t)nixl_peer_info[idx].sync_buffer_ptr, max_num_ranks * sizeof(int), nixl_peer_info[idx].device_id, nixl_agent_info->remote_agent_names[idx]));
     }
 
     nixl_opt_args_t opt_args;
